92b18f50403efddd4906e4fac5f67c33516cb202
frinhanifrinhani <frinhani.frinhani@gmail.com>
2019-10-17 21:51:52 -0300

    Busca de produtos por nome e categorias

--- src/main/java/br/com/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/br/com/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,34 @@
 package br.com.cursomc.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.cursomc.domain.Categoria;
 import br.com.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	/*
+	 * using query JPQL
+	@Query(
+			"SELECT "
+			+ "DISTINCT obj "
+			+ "FROM Produto obj "
+			+ "INNER JOIN obj.categorias cat "
+			+ "WHERE obj.nome LIKE %:nome% "
+			+ "AND cat IN :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	*/
+	
+	// using method form spring
+	@Transactional(readOnly = true)
+	public Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
 }
---
+++ src/main/java/br/com/cursomc/services/ProdutoService.java
@@ -0,0 +1,44 @@
+package br.com.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.domain.Produto;
+import br.com.cursomc.repositories.CategoriaRepository;
+import br.com.cursomc.repositories.ProdutoRepository;
+import br.com.cursomc.services.exceptions.ObjectNotFoundExecption;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		
+		Optional<Produto> produto = produtoRepository.findById(id);
+		
+		return produto.orElseThrow(() -> new ObjectNotFoundExecption(
+				"Objeto não encontrado! Id: " + id + ", Tipo: "+ Produto.class.getName()));
+		
+	}
+	
+	public Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		
+		return produtoRepository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+}
